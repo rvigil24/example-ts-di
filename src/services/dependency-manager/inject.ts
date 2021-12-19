e45a0db68793b908e6f38a0a6f722617e07fcf54
Ruben Vigil <ruben.vigil24@gmail.com>
2021-12-18 22:53:42 -0600

    Create Inject decorator
    This decorator takes a key and search it in the dependency container, if the dependency key is found, will return the dependency already instantiated and will add it to the property below the decorator

---
+++ src/services/dependency-manager/inject.ts
@@ -0,0 +1,11 @@
+import { dependencyContainer } from "./dependencies-container";
+
+export const Inject = (key: string) => {
+  return (classToInject: any, property: string) => {
+    Object.defineProperty(classToInject, property, {
+      get: () => dependencyContainer.get(key),
+      enumerable: true,
+      configurable: true,
+    });
+  };
+};
